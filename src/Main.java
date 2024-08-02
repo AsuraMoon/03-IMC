42482a2442b08fc14ddee87cc336c8a487383f8c
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-08-02 09:48:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Corpulence bibs = new Corpulence(175, 80);
+		Corpulence bibs = new Corpulence(185, 71);
 		System.out.println(bibs.getTaille());
 		System.out.println(bibs.getPoids());
 		System.out.println(bibs.calculIMC());
